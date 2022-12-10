9f61d452a6e9fb39fd7755e5e9372097f4b3e085
matamata-dayo <k.matamata.macbook@icloud.com>
2022-12-10 21:39:33 +0900

    natureremo

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+// func main() {
+// 	cli := natureremo.NewClient(os.Args[1])
+// 	ctx := context.Background()
+
+// 	applianceName := os.Args[2]
+// 	signalName := os.Args[3]
+
+// 	as, err := cli.ApplianceService.GetAll(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	var target *natureremo.Appliance
+// 	for _, a := range as {
+// 		if a.Nickname == applianceName {
+// 			target = a
+// 			break
+// 		}
+// 	}
+
+// 	if target == nil {
+// 		log.Fatalf("%s not found", applianceName)
+// 	}
+
+// 	for _, s := range target.Signals {
+// 		if s.Name == signalName {
+// 			cli.SignalService.Send(ctx, s)
+// 			break
+// 		}
+// 	}
+// }
